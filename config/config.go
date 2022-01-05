807d7fe78f5b5da741a773886a564c70c8e397e4
TMNT <trietpersonal@gmail.com>
2022-01-04 22:58:45 -0500

    changed config fields

--- config/config.go
+++ config/config.go
@@ -1,17 +1,22 @@
 package config
 
 import (
-	"log"
-
-	"github.com/kelseyhightower/envconfig"
+	"encoding/json"
+	"io/ioutil"
+	"os"
 )
 
 // type IConfigProvider interface {
 // 	ProvideConfig() config
 // }
 
+// type config struct {
+// 	Bucket, DataSource, RepoName string
+// }
+
 type config struct {
-	Bucket, DataSource, RepoName string
+	Bucket string `json:"bucket"`
+	Repo   string `json:"repo"`
 }
 
 // type ApplicationConfig struct {
@@ -20,20 +25,28 @@ type config struct {
 // }
 
 // Load config from env vars
-func NewConfig() config {
-	var c config
-	if err := envconfig.Process("", &c); err != nil {
-		log.Fatal(err.Error())
+// func NewConfig() config {
+// 	var c config
+// 	if err := envconfig.Process("", &c); err != nil {
+// 		log.Fatal(err.Error())
+// 	}
+// 	return c
+// }
+
+// Load config from config.json
+func NewConfig(path string) (config, error) {
+	jsonFile, err := os.Open(path)
+	if err != nil {
+		return config{}, err
 	}
-	return c
+	defer jsonFile.Close()
+
+	jsonByte, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return config{}, err
+	}
+
+	var c config
+	json.Unmarshal(jsonByte, &c)
+	return c, err
 }
-
-// func GetConfig() (Config, error) {
-// 	sess, err := session.NewSession()
-// 	svc := session.Must(sess, err)
-// 	ssmsvc := &SSM{ssm.New(sess)}
-// }
-
-// func (c Config) String() {
-// 	return string(c)
-// }
