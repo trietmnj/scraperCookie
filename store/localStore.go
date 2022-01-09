398021c0a4125fa266b91b3f20009692ce3a2c4d
TMNT <trietpersonal@gmail.com>
2022-01-09 15:08:21 -0500

    parse env var working in local store

--- store/localStore.go
+++ store/localStore.go
@@ -9,19 +9,23 @@ import (
 	"github.com/kelseyhightower/envconfig"
 )
 
+// LOCAL_STOREPATH has to be available as an env var
 type localStore struct {
-	storePath string `envconfig:""`
+	StorePath string `envconfig:"storepath" required:"true"` // field name has to be exportable to work with envconfig
 	file      *os.File
 }
 
 // Init() generates StorePath from env var LOCAL_STOREPATH
 func (s *localStore) Init() {
-	var c localStore
-	// TODO something wrong with processing env var to configs
+	var c localStore // temporary var to work with envconfig instead of direct mutation on reciever
 	err := envconfig.Process("local", &c)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	if c.StorePath == "" {
+		log.Fatal("local store: unable to parse config from env")
+	}
+	s.StorePath = c.StorePath
 }
 
 // Read is unimplemented
@@ -30,7 +34,7 @@ func (s *localStore) Read(l Locator) []byte {
 }
 
 func (s *localStore) Store(l Locator, data io.Reader) error {
-	file, err := os.Create(s.storePath + l.Key)
+	file, err := os.Create(s.StorePath + l.Key)
 	if err != nil {
 		return err
 	}
