70d44d5841553085c690a72ee61c755913b27d7f
TMNT <trietpersonal@gmail.com>
2022-01-23 17:42:20 -0500

    exported localStore

--- store/localStore.go
+++ store/localStore.go
@@ -14,16 +14,16 @@ import (
 	"github.com/trietmnj/scraperCookie/utils"
 )
 
-// TODO should local store really take config from env
 // LOCAL_STOREPATH has to be available as an env var
-type localStore struct {
+// TODO should local store really take config from env
+type LocalStore struct {
 	StorePath string `envconfig:"storepath" required:"true"` // field name has to be exportable to work with envconfig
 	file      *os.File
 }
 
 // Init() generates StorePath from env var LOCAL_STOREPATH
-func (s *localStore) Init() {
-	var c localStore // temporary var to work with envconfig instead of direct mutation on reciever
+func (s *LocalStore) Init() {
+	var c LocalStore // temporary var to work with envconfig instead of direct mutation on reciever
 	err := envconfig.Process("local", &c)
 	if err != nil {
 		log.Fatal(err.Error())
@@ -34,12 +34,12 @@ func (s *localStore) Init() {
 	s.StorePath = c.StorePath
 }
 
-// Read is unimplemented
-func (s *localStore) Read(l Locator) []byte {
-	return []byte{}
+func (s *LocalStore) Read(l Locator) ([]byte, error) {
+	filePath := filepath.Join(s.StorePath, l.Bucket, l.Key)
+	return os.ReadFile(filePath)
 }
 
-func (s *localStore) Store(l Locator, data io.Reader) error {
+func (s *LocalStore) Store(l Locator, data io.Reader) error {
 	filePath := filepath.Join(s.StorePath, l.Bucket, l.Key)
 	err := os.MkdirAll(utils.Path(filePath), os.ModePerm)
 	file, err := os.Create(filePath)
@@ -57,7 +57,7 @@ func (s *localStore) Store(l Locator, data io.Reader) error {
 	return err
 }
 
-func (s *localStore) KeyExists(l Locator) (bool, error) {
+func (s *LocalStore) KeyExists(l Locator) (bool, error) {
 	filePath := filepath.Join(s.StorePath, l.Bucket, l.Key)
 	if _, err := os.Stat(filePath); err == nil {
 		return true, nil
@@ -68,14 +68,17 @@ func (s *localStore) KeyExists(l Locator) (bool, error) {
 }
 
 // List returns a list of files at the locator. Input Locator key has to be a folder.
-func (s *localStore) List(l Locator) ([]Locator, error) {
+func (s *LocalStore) List(l Locator) ([]Locator, error) {
+	// TODO validate input locator is a folder
 	var files []string
 	root := filepath.Join(s.StorePath, l.Bucket, l.Key)
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if err == nil && !info.IsDir() {
+		if err != nil {
+			return err
+		} else if !info.IsDir() {
 			files = append(files, path)
 		}
-		return err
+		return nil
 	})
 
 	var ls []Locator
