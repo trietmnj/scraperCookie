c660f0dd5d373a5d6891d2938b43ef154cb5b240
TMNT <trietpersonal@gmail.com>
2022-01-29 20:31:15 -0500

    fixed json struct parser typo

--- pkg/configs/configs.go
+++ pkg/configs/configs.go
@@ -9,11 +9,10 @@ import (
 type Config struct {
 	Bucket    string `json:"bucket"`
 	Repo      string `json:"repo"`
-	LocalPath string `json"localPath"`
+	LocalPath string `json:"local-path"`
 }
 
 // Load config from config.json
-// TODO refactor config to select between env var and json
 func NewConfig(path string) (Config, error) {
 	jsonFile, err := os.Open(path)
 	if err != nil {
