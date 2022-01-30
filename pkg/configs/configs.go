b68fe807bf836a1c25900c4f1c0806f1df1ecbc5
TMNT <trietpersonal@gmail.com>
2022-01-29 20:25:15 -0500

    cleaning up configs

--- pkg/configs/configs.go
+++ pkg/configs/configs.go
@@ -1,4 +1,4 @@
-package config
+package configs
 
 import (
 	"encoding/json"
@@ -6,33 +6,11 @@ import (
 	"os"
 )
 
-// type IConfigProvider interface {
-// 	ProvideConfig() config
-// }
-
-// type config struct {
-// 	Bucket, DataSource, RepoName string
-// }
-
 type Config struct {
 	Bucket    string `json:"bucket"`
 	Repo      string `json:"repo"`
 	LocalPath string `json"localPath"`
 }
-
-// type ApplicationConfig struct {
-// 	AwsRegion string `json:"awsRegion"`
-// 	AwsRegion string `json:"awsRegion"`
-// }
-
-// Load config from env vars
-// func NewConfig() config {
-// 	var c config
-// 	if err := envconfig.Process("", &c); err != nil {
-// 		log.Fatal(err.Error())
-// 	}
-// 	return c
-// }
 
 // Load config from config.json
 // TODO refactor config to select between env var and json
