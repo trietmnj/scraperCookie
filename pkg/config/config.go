3b6d5dcc7c026d475a9cb42fbfc1d19686a22753
TMNT <trietpersonal@gmail.com>
2022-02-15 21:19:00 -0500

    fixed json unmarshal config bug

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -10,17 +10,17 @@ import (
 )
 
 type AppConfig struct {
-	ConfigType  types.ConfigSource
-	JsonPath    string // path to config json
-	StoreConfig        // store specific
+	ConfigType types.ConfigSource
+	JsonPath   string // path to config json
+	StoreConfig
 }
 
 type StoreConfig struct {
-	S3Store      S3StoreConfig    `json:"s3store"`
-	LocalStore   LocalStoreConfig `json:"localstore"`
-	Repo         string           `json:"repo"`
-	Proxy        bool             `json:"proxy"`
-	StoreVariant store.Variant    `json:"storevariant"`
+	S3Store    S3StoreConfig    `json:"s3store"`
+	LocalStore LocalStoreConfig `json:"localstore"`
+	Repo       string           `json:"repo"`
+	Proxy      bool             `json:"proxy"`
+	StoreType  types.Store      `json:"storetype"`
 }
 
 type S3StoreConfig struct {
@@ -32,30 +32,31 @@ type LocalStoreConfig struct {
 }
 
 // path - path to json config file
-func NewConfig(v Variant, path string) (AppConfig, error) {
+func NewConfig(v types.ConfigSource, path string) (AppConfig, error) {
 	var sc StoreConfig
 	var err error
 	switch v {
-	case Json:
+	case types.Json:
+		if path == "" {
+			return AppConfig{}, errors.New("invalid json file path: " + path)
+		}
 		jsonFile, err := os.Open(path)
 		if err != nil {
 			return AppConfig{}, err
 		}
 		defer jsonFile.Close()
-
 		jsonByte, err := ioutil.ReadAll(jsonFile)
 		if err != nil {
 			return AppConfig{}, err
 		}
-
-		var sc StoreConfig
 		json.Unmarshal(jsonByte, &sc)
+
 	default:
 		return AppConfig{}, errors.New("invalid config variant")
 	}
 	return AppConfig{
-		configVariant: v,
-		JsonPath:      path,
-		StoreConfig:   sc,
+		ConfigType:  v,
+		JsonPath:    path,
+		StoreConfig: sc,
 	}, err
 }
