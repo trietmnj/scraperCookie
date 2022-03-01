06852e79914a067cd8e56e53e3ab40374b88a6d4
TMNT <trietpersonal@gmail.com>
2022-03-01 04:24:08 -0500

    cleaning up config test

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -4,17 +4,18 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/trietmnj/scraperCookie/internal/types"
 )
 
 func TestNewConfigLocal(t *testing.T) {
 	c, err := NewConfig(types.JsonConfigSource, "/workspaces/scraperCookie/test/config-local.json")
 	fmt.Println(c)
-	fmt.Println(err)
+	assert.Nil(t, err)
 }
 
 func TestNewConfigS3(t *testing.T) {
 	c, err := NewConfig(types.JsonConfigSource, "/workspaces/scraperCookie/test/config-s3.json")
 	fmt.Println(c)
-	fmt.Println(err)
+	assert.Nil(t, err)
 }
